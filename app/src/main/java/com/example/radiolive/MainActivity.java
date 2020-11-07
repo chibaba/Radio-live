f799436c1b276ff4c24bb00f9d24a38ce1659396
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-07 03:07:00 +0100

    wip

--- app/src/main/java/com/example/radiolive/MainActivity.java
+++ app/src/main/java/com/example/radiolive/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.ui.PlayerView;
 
@@ -13,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     PlayerView playerView;
     SimpleExoPlayer myExoPlayer;
+    ProgressBar progressBar;
+
+    Player.EventListener listener;
 
     String radio_url = "http://voa28.akacast.akamaistream.net/7/325/437810/v1/ibb.akacast.akamaistream.net/voa28";
 
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         playerView = findViewById(R.id.player_view);
+        progressBar = findViewById(R.id.progress_bar);
 
         initializePlayer();
     }
@@ -36,5 +43,24 @@ public class MainActivity extends AppCompatActivity {
 
         myExoPlayer.prepare();
         myExoPlayer.play();
+
+        listener = new Player.EventListener() {
+            @Override
+            public void onPlaybackStateChanged(int state) {
+               if(state == Player.STATE_BUFFERING) {
+                   progressBar.setVisibility(View.VISIBLE);
+               }
+
+               if(state == Player.STATE_READY) {
+                   progressBar.setVisibility(View.GONE);
+               }
+            }
+        };
+        myExoPlayer.addListener(listener);
         }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
