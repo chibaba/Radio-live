524defec48ec58cfd4dd95d7cf22a7b4509e76e5
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-07 01:57:07 +0100

    wip

--- app/src/main/java/com/example/radiolive/MainActivity.java
+++ app/src/main/java/com/example/radiolive/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+
 public class MainActivity extends AppCompatActivity {
+
+    PlayerView playerView;
+    SimpleExoPlayer myExoPlayer;
+
+    String radio_url = "http://voa28.akacast.akamaistream.net/7/325/437810/v1/ibb.akacast.akamaistream.net/voa28";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerView = findViewById(R.id.player_view);
+
+        initializePlayer();
+    }
+
+    private void initializePlayer() {
+
     }
 }
